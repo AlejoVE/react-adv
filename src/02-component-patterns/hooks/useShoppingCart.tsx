b0b5c2650d26a56d3e2b8d1c9b1fdca99e10748b
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2023-07-20 19:17:00 +0200

    Implement Control Props alt

--- src/02-component-patterns/hooks/useShoppingCart.tsx
+++ src/02-component-patterns/hooks/useShoppingCart.tsx
@@ -6,29 +6,14 @@ export const useShoppingCart = () => {
 
 	const onProductChange = ({ count, product }: { product: Product; count: number }) => {
 		setShoppingCart((oldShoppingCart) => {
-			// if (count === 0) {
-			// 	const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-			// 	return { ...rest };
-			// }
-			// return {
-			// 	...oldShoppingCart,
-			// 	[product.id]: { ...product, count },
-			// };
-
-			const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
-
-			if (Math.max(productInCart.count + count, 0) > 0) {
-				productInCart.count += count;
-				return {
-					...oldShoppingCart,
-					[product.id]: productInCart,
-				};
+			if (count === 0) {
+				const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+				return { ...rest };
 			}
-
-			// Delete product
-
-			const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-			return { ...rest };
+			return {
+				...oldShoppingCart,
+				[product.id]: { ...product, count },
+			};
 		});
 	};
 
